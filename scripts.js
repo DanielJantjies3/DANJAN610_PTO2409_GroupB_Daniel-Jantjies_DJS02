78d08cd599e80f2954e9ed27adc9fd8e7cbec4b6
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-05-04 22:21:49 +0200

    Checking Incomplete Inputs

--- scripts.js
+++ scripts.js
@@ -5,5 +5,11 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+//checking incomplete/empty inputs
+  if(!dividend || !divider) {
+  result.innerText = "Division not performed. Both values are required in inputs. Try again."
+  return;
+}
+
 });
